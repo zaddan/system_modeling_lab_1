2353f83133df4df28b94152392c2ab0ec266bf7b
zaddan <behzadboro@gmail.com>
2015-09-20 12:48:55 -0500

    get_image done

---
+++ SUSAN/get_int.c
@@ -0,0 +1,31 @@
+#include "config.h"
+
+int getint(fd)
+  FILE *fd;
+{
+  int c, i;
+  char dummy[10000];
+
+  c = getc(fd);
+  while (1) /* find next integer */
+  {
+    if (c=='#')    /* if we're at a comment, read to end of line */
+      fgets(dummy,9000,fd);
+    if (c==EOF)
+      exit_error("Image %s not binary PGM.\n","is");
+    if (c>='0' && c<='9')
+      break;   /* found what we were looking for */
+    c = getc(fd);
+  }
+
+  /* we're at the start of a number, continue until we hit a non-number */
+  i = 0;
+  while (1) {
+    i = (i*10) + (c - '0');
+    c = getc(fd);
+    if (c==EOF) return (i);
+    if (c<'0' || c>'9') break;
+  }
+
+  return (i);
+}
--- SUSAN/susan.c
+++ SUSAN/susan.c
@@ -303,8 +303,7 @@ typedef float      TOTAL_TYPE; /* for my PowerPC accelerator only */
 #include <sys/file.h>    /* may want to remove this line */
 #include <malloc.h>      /* may want to remove this line */
 #include "config.h"
-#define  exit_error(IFB,IFC) { fprintf(stderr,IFB,IFC); exit(0); }
-#define  FTOI(a) ( (a) < 0 ? ((int)(a-0.5)) : ((int)(a+0.5)) )
+
 
 typedef  struct {int x,y,info, dx, dy, I;} CORNER_LIST[MAX_CORNERS];
 
@@ -338,74 +337,11 @@ usage()
 
 /* {{{ int getint(fp) derived from XV */
 
-int getint(fd)
-  FILE *fd;
-{
-  int c, i;
-  char dummy[10000];
 
-  c = getc(fd);
-  while (1) /* find next integer */
-  {
-    if (c=='#')    /* if we're at a comment, read to end of line */
-      fgets(dummy,9000,fd);
-    if (c==EOF)
-      exit_error("Image %s not binary PGM.\n","is");
-    if (c>='0' && c<='9')
-      break;   /* found what we were looking for */
-    c = getc(fd);
-  }
-
-  /* we're at the start of a number, continue until we hit a non-number */
-  i = 0;
-  while (1) {
-    i = (i*10) + (c - '0');
-    c = getc(fd);
-    if (c==EOF) return (i);
-    if (c<'0' || c>'9') break;
-  }
-
-  return (i);
-}
 
 /* }}} */
 //need this one
-void get_image(filename,in_global,x_size,y_size)
-  char           filename[200];
-  unsigned char  *in_global;
-  int            *x_size, *y_size;
-{
-FILE  *fd;
-char header [100];
-int  tmp;
-unsigned char **in = &in_global;
-#ifdef FOPENB
-  if ((fd=fopen(filename,"rb")) == NULL)
-#else
-  if ((fd=fopen(filename,"r")) == NULL)
-#endif
-    exit_error("Can't input image %s.\n",filename);
 
-  /* {{{ read header */
-
-  header[0]=fgetc(fd);
-  header[1]=fgetc(fd);
-  if(!(header[0]=='P' && header[1]=='5'))
-    exit_error("Image %s does not have binary PGM header.\n",filename);
-
-  *x_size = getint(fd);
-  *y_size = getint(fd);
-  tmp = getint(fd);
-
-/* }}} */
-
-  //*in = (uchar *) malloc(*x_size * *y_size);
-
-  if (fread(*in,1,*x_size * *y_size,fd) == 0)
-    exit_error("Image %s is wrong size.\n",filename);
-
-  fclose(fd);
-}
 
 /* }}} */
 /* {{{ put_image(filename,in,x_size,y_size) */
