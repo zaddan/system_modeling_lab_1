c54f3b70c340868976a765be127fed2a13aee92e
zaddan <behzadboro@gmail.com>
2015-09-20 11:24:17 -0500

    blah

--- SUSAN/config.h
+++ SUSAN/config.h
@@ -1,3 +1,6 @@
 #define BP_CONST 516 //migh have to modify later
 #define X_SIZE_CONST 76
 #define Y_SIZE_CONST 95
+typedef  unsigned char uchar;
+void susan_thin(int r[],uchar mid[],int x_size,int y_size);
+void edge_draw(uchar *in, uchar *mid,int x_size,int y_size,int drawing_mode);
