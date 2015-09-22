7fc909617f8f33ad75d7d1da50dc3713b2637a52
Behzad Boroujerdian <behzadboro@gmail.com>
2015-09-22 17:48:20 -0500

    more polishing

--- SUSAN_SC/constant.hh
+++
@@ -1,7 +0,0 @@
-#define BP_CONST 516 //migh have to modify later
-#define X_SIZE_CONST 76
-#define Y_SIZE_CONST 95
-#define IMAGE_SIZE X_SIZE_CONST*Y_SIZE_CONST
-#define FILE_NAME_SIZE 100 
-#define MAX_NO_EDGES 2650
-//typedef const unsigned long size
