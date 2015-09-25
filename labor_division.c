defcda0e6570acdf442b6d9b1df02759dcc7d691
Behzad Boroujerdian <behzadboro@gmail.com>
2015-09-24 19:21:05 -0500

    labor_div updated

--- labor_division.c
+++ labor_division.c
@@ -1,7 +1,7 @@
 0. make trigger (start signal) a double handshake , right now it is queue of size 1
 1. modify stimulus.sc 
-2. create read_image.sc
-3. create put image.sc
+2. create read_image.sc (kishore)
+3. create put image.sc (behzad)
 
 //group, each can be individually tested 
 1 - task 0,1
